24ac20be06c9b9669e2699e13e05a5a13f588c55
RedDotz20 <carlos_tabangay@outlook.com>
2023-12-15 16:17:46 +0800

    update: login-register UI

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,8 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:todo_app/services/firestore.dart';
+import 'package:todo_app/services/auth.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,11 +12,22 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String? errorMessage = "";
   // firestore
   final FirestoreService firestoreService = FirestoreService();
 
   // text controller
   final TextEditingController textController = TextEditingController();
+
+  Future<void> signOutAccount() async {
+    try {
+      await Auth().signOut();
+    } on FirebaseAuthException catch (e) {
+      setState(() {
+        errorMessage = e.message;
+      });
+    }
+  }
 
   // open dialog box to add note
   void openNoteBox({String? docID}) {
@@ -57,59 +70,92 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Notes")),
+      drawer: Drawer(
+        child: Column(
+          children: <Widget>[
+            const UserAccountsDrawerHeader(
+              accountName: Text("User Name"),
+              accountEmail: Text("user@example.com"),
+              currentAccountPicture: CircleAvatar(
+                backgroundColor: Colors.white,
+                child: Icon(Icons.person),
+              ),
+            ),
+            ListTile(
+              title: const Text('Item 1'),
+              onTap: () {
+                // Handle item 1 press
+              },
+            ),
+            ListTile(
+              title: const Text('Item 2'),
+              onTap: () {
+                // Handle item 2 press
+              },
+            ),
+            const Spacer(),
+            ListTile(
+              title: const Text('Logout'),
+              onTap: () {
+                // Handle logout press
+                signOutAccount();
+                // You can add your logout logic here
+              },
+            ),
+          ],
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => openNoteBox(),
         child: const Icon(Icons.add),
       ),
       body: StreamBuilder<QuerySnapshot>(
-          stream: firestoreService.getNotesStream(),
-          builder: (context, snapshot) {
-            // if we have data, get all the docs
-            if (snapshot.hasData) {
-              List notesList = snapshot.data!.docs;
+        stream: firestoreService.getNotesStream(),
+        builder: (context, snapshot) {
+          // if we have data, get all the docs
+          if (snapshot.hasData) {
+            List notesList = snapshot.data!.docs;
 
-              // display as a list
-              return ListView.builder(
-                  itemCount: notesList.length,
-                  itemBuilder: (context, index) {
-                    // get each individual doc
-                    DocumentSnapshot document = notesList[index];
-                    String docID = document.id;
+            // display as a list
+            return ListView.builder(
+              itemCount: notesList.length,
+              itemBuilder: (context, index) {
+                // get each individual doc
+                DocumentSnapshot document = notesList[index];
+                String docID = document.id;
 
-                    // get note from each doc
-                    Map<String, dynamic> data =
-                        document.data() as Map<String, dynamic>;
-                    String noteText = data['note'];
+                // get note from each doc
+                Map<String, dynamic> data =
+                    document.data() as Map<String, dynamic>;
+                String noteText = data['note'];
 
-                    // display as a list title
-                    return ListTile(
-                      title: Text(noteText),
-                      trailing: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          // update button
-                          IconButton(
-                              onPressed: () => openNoteBox(docID: docID),
-                              icon: const Icon(Icons.settings)),
+                // display as a list title
+                return ListTile(
+                  title: Text(noteText),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      // update button
+                      IconButton(
+                          onPressed: () => openNoteBox(docID: docID),
+                          icon: const Icon(Icons.settings)),
 
-                          // delete button
-                          IconButton(
-                              onPressed: () => firestoreService.deleteNote(
-                                    docID,
-                                  ),
-                              icon: const Icon(Icons.delete)),
-                        ],
-                      ),
-
-                      // trailing: IconButton(
-                      //     onPressed: () => openNoteBox(docID: docID),
-                      //     icon: const Icon(Icons.settings)),
-                    );
-                  });
-            } else {
-              return const Text("No Notes...");
-            }
-          }),
+                      // delete button
+                      IconButton(
+                          onPressed: () => firestoreService.deleteNote(
+                                docID,
+                              ),
+                          icon: const Icon(Icons.delete)),
+                    ],
+                  ),
+                );
+              },
+            );
+          } else {
+            return const Text("No Notes...");
+          }
+        },
+      ),
     );
   }
 }
--- lib/pages/login_register_page.dart
+++ lib/pages/login_register_page.dart
@@ -12,56 +12,126 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   String? errorMessage = "";
   bool isLogin = true;
+  bool obscureText = true;
 
   final TextEditingController _controllerEmail = TextEditingController();
   final TextEditingController _controllerPassword = TextEditingController();
 
   Future<void> signInWithEmailAndPassword() async {
+    final BuildContext currentContext = context; //
     try {
       await Auth().signInWithEmailAndPassword(
           email: _controllerEmail.text, password: _controllerPassword.text);
     } on FirebaseAuthException catch (e) {
-      setState(() {
-        errorMessage = e.message;
+      Future.delayed(Duration.zero, () {
+        ScaffoldMessenger.of(currentContext).showSnackBar(
+          SnackBar(
+            content: Text("Error: ${e.message}"),
+          ),
+        );
       });
     }
   }
 
   Future<void> createUserWithEmailAndPassword() async {
+    final BuildContext currentContext = context; // Store the context
+
     try {
       await Auth().createUserWithEmailAndPassword(
           email: _controllerEmail.text, password: _controllerPassword.text);
     } on FirebaseAuthException catch (e) {
-      setState(() {
-        errorMessage = e.message;
+      Future.delayed(Duration.zero, () {
+        ScaffoldMessenger.of(currentContext).showSnackBar(
+          SnackBar(
+            content: Text("Error: ${e.message}"),
+          ),
+        );
       });
     }
   }
 
-  Widget _title() {
-    return const Text("Firebase Auth");
-  }
-
   Widget _entryField(String title, TextEditingController controller) {
-    return TextField(
-      controller: controller,
-      decoration: InputDecoration(labelText: title),
+    bool isPassword = title.toLowerCase() == 'password';
+
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 16.0),
+      child: TextField(
+        controller: controller,
+        style: const TextStyle(color: Colors.white),
+        obscureText: isPassword ? obscureText : false,
+        cursorColor: Colors.blue,
+        decoration: InputDecoration(
+          labelText: title,
+          labelStyle: const TextStyle(color: Colors.white),
+          hintText: 'Enter your $title',
+          hintStyle: const TextStyle(color: Colors.grey),
+          enabledBorder: OutlineInputBorder(
+            borderSide: const BorderSide(color: Colors.white),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: const BorderSide(color: Colors.white),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          suffixIcon: isPassword
+              ? IconButton(
+                  onPressed: () {
+                    setState(() {
+                      obscureText = !obscureText;
+                    });
+                  },
+                  icon: Icon(
+                    obscureText ? Icons.visibility : Icons.visibility_off,
+                    color: Colors.white,
+                  ),
+                )
+              : null,
+        ),
+      ),
     );
   }
 
-  Widget _errorMessage() {
-    return Text(errorMessage == "" ? "" : "Error: $errorMessage");
+  Widget _authStatusText() {
+    return SizedBox(
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.only(bottom: 20.0),
+        child: Text(
+          isLogin ? "Login Your Account" : "Create your account",
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
   }
 
   Widget _submitButton() {
-    return ElevatedButton(
-      onPressed: () {
-        isLogin
-            ? signInWithEmailAndPassword()
-            : createUserWithEmailAndPassword();
-      },
-      child: Text(isLogin ? "Login" : "Register"),
-    );
+    return SizedBox(
+        width: double.infinity,
+        child: ElevatedButton(
+          onPressed: () {
+            isLogin
+                ? signInWithEmailAndPassword()
+                : createUserWithEmailAndPassword();
+          },
+          style: ElevatedButton.styleFrom(
+            shape: RoundedRectangleBorder(
+              borderRadius:
+                  BorderRadius.circular(10), // Adjust the border radius
+            ),
+          ),
+          child: Text(
+            isLogin ? "LOGIN" : "REGISTER",
+            style: const TextStyle(
+              color: Colors.black,
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ));
   }
 
   Widget _loginOrRegisterButton() {
@@ -71,31 +141,70 @@ class _LoginPageState extends State<LoginPage> {
           isLogin = !isLogin;
         });
       },
-      child: Text(isLogin
-          ? "Don't have an account? Register here"
-          : "Already have an account? Login here"),
+      child: Text(
+        isLogin
+            ? "Don't have an account? Register here"
+            : "Already have an account? Login here",
+        style: const TextStyle(
+          color: Colors.blue,
+          fontSize: 16.0,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+
+  Widget _headerIcon() {
+    return Container(
+      padding: const EdgeInsets.only(bottom: 20.0),
+      child: const Icon(
+        Icons.edit_note,
+        size: 100.0,
+        color: Colors.white,
+      ),
+    );
+  }
+
+  Widget _subHeader() {
+    return Container(
+      padding: const EdgeInsets.only(bottom: 40.0),
+      child: const Text(
+        "Notes App",
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 30.0,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: _title()),
-        body: Container(
-          height: double.infinity,
-          width: double.infinity,
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              _entryField("Email", _controllerEmail),
-              _entryField("Password", _controllerPassword),
-              _errorMessage(),
-              _submitButton(),
-              _loginOrRegisterButton(),
-            ],
-          ),
-        ));
+        backgroundColor: Colors.black,
+        // appBar: AppBar(title: _title()),
+        body: Padding(
+            padding: const EdgeInsets.symmetric(
+                horizontal: 15.0), // Adjust as needed
+            child: Container(
+              height: double.infinity,
+              width: double.infinity,
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  _headerIcon(),
+                  _subHeader(),
+                  _authStatusText(),
+                  _entryField("Email", _controllerEmail),
+                  _entryField("Password", _controllerPassword),
+                  // _errorMessage(),
+                  _submitButton(),
+                  _loginOrRegisterButton(),
+                ],
+              ),
+            )));
   }
 }
